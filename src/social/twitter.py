58d6ed3c8f965ac4078b0e9d79551dd3c7cfbc36
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2025-03-18 02:52:34 +0300

    Add Facebook and Twitter automation handlers for posting updates with images

---
+++ src/social/twitter.py
@@ -0,0 +1,19 @@
+import tweepy
+
+class TwitterHandler:
+    def __init__(self, consumer_key, consumer_secret, access_token, access_secret):
+        auth = tweepy.OAuth1UserHandler(
+            consumer_key, consumer_secret, access_token, access_secret
+        )
+        self.api = tweepy.API(auth)
+
+    def post(self, message, image_path=None):
+        try:
+            if image_path:
+                media = self.api.media_upload(image_path)
+                self.api.update_status(status=message, media_ids=[media.media_id])
+            else:
+                self.api.update_status(status=message)
+            print("Twitter post successful!")
+        except tweepy.TweepError as e:
+            print(f"Twitter post failed: {e}")
